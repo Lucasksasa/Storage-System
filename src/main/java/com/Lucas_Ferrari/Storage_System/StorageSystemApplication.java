9cc851f66f2649f5aeff37fb2b1a82629a11dbbb
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-15 14:36:14 -0300

    first commit

---
+++ src/main/java/com/Lucas_Ferrari/Storage_System/StorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Lucas_Ferrari.Storage_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageSystemApplication.class, args);
+	}
+
+}
