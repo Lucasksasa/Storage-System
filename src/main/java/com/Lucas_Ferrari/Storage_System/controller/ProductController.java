aa1af395ae38f483b4a183f1f0eec21e34a45f54
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-15 15:53:30 -0300

    feat: implement the GET Methods, by ID and a List of all products

--- src/main/java/com/Lucas_Ferrari/Storage_System/controller/ProductController.java
+++ src/main/java/com/Lucas_Ferrari/Storage_System/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.Lucas_Ferrari.Storage_System.model.Product;
 import com.Lucas_Ferrari.Storage_System.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -17,6 +20,16 @@ public class ProductController {
     @PostMapping
     public Product addProduct(@RequestBody Product product){
         return productService.addProduct(product);
+    }
+
+    @GetMapping("{id}")
+    public Optional<Product> productByID(@PathVariable Long id){
+        return productService.listProductsById(id);
+    }
+
+    @GetMapping
+    public List<Product> listOfProducts(){
+        return productService.listAllProducts();
     }
 
 }
