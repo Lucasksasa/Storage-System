fe0146851513c3108d967613684370bb1d2842a5
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-15 14:51:40 -0300

    feat: create The repository of Product, extending JPA

---
+++ src/main/java/com/Lucas_Ferrari/Storage_System/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.Lucas_Ferrari.Storage_System.repository;
+
+import com.Lucas_Ferrari.Storage_System.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Long, Product> {
+
+}
