114ff4e769ace6af8a8c5c8e007aa5f4ec6e95b0
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-15 15:00:20 -0300

    chore: reorganize the parameters added to the repository

--- src/main/java/com/Lucas_Ferrari/Storage_System/repository/ProductRepository.java
+++ src/main/java/com/Lucas_Ferrari/Storage_System/repository/ProductRepository.java
@@ -3,6 +3,6 @@ package com.Lucas_Ferrari.Storage_System.repository;
 import com.Lucas_Ferrari.Storage_System.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Long, Product> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
