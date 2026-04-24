fb50497ccc464c499baf7b6da2708a314220c1a2
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-24 19:07:56 -0300

    feat: create class User

---
+++ src/main/java/com/Lucas_Ferrari/Storage_System/model/User.java
@@ -0,0 +1,24 @@
+package com.Lucas_Ferrari.Storage_System.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double cpf;
+    private String cellNumber;
+
+}
