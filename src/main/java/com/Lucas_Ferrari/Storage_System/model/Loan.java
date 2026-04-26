ac378c1daadf19c91e3d65c20822c7a1e77f79a7
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-04-26 10:11:31 -0300

    chore: create Loan class to start the conection between user and product

---
+++ src/main/java/com/Lucas_Ferrari/Storage_System/model/Loan.java
@@ -0,0 +1,30 @@
+package com.Lucas_Ferrari.Storage_System.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+
+    private LocalDateTime date;
+}
